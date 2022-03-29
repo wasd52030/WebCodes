4215f952e2e62c49b4b65a1545bf413bd7b16800
wasd52030 <71220988+wasd52030@users.noreply.github.com>
2022-03-29 17:21:18 +0800

    no message

---
+++ React Practice/20220328/src/Components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from '../Counter/Counter'
+import Menu from '../Menu/Menu'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Menu />
+    </>
+  )
+}
+
+export default App
---
+++ React Practice/20220328/src/Components/Menu/Menu.tsx
@@ -0,0 +1,15 @@
+import menus from "./Data.json"
+import MenuItem from "./Menuitem"
+import "./Menu.css"
+
+export default function Menu() {
+    return (
+        <div style={{ display: "flex",flexWrap:"wrap"}}>
+            {
+                menus.meals.map((item, index) => {
+                    return <MenuItem key={index} meal={item} />
+                })
+            }
+        </div>
+    )
+}
