058a1e9f7ca484ec7c149fd76357bbc7675fc6e1
sobol <nppstcc1001050311@gmail.com>
2022-08-15 10:45:10 +0800

    no message

--- 20210308/1/main.js
+++ 20210308/1/main.js
@@ -1,41 +1,41 @@
 $(document).ready(function () {
     $("#btn1").click(function (e) {
-        let check=new RegExp('^\\d+$');
-        let Din=$("#input1").val();
-        if(check.test(Din))
+        let Din=$("#input1").val()
+        if(/^\d+$/.test(Din))
         {
-            Din=parseInt(Din);
-            let o=PrintTrangle(Din);
-            $("#out").html(o);
+            Din=parseInt(Din)
+            let o=PrintTrangle(Din)
+            $("#out").html(o)
         }
         else
-            alert("輸入錯誤，只能入數字");
-    });
-});
+            alert("輸入錯誤，只能入數字")
+    })
+})
 
 function nCr(n,r)
 {
     if(r==0||n==r)
-        return 1;
+        return 1
     else
-        return nCr(n-1,r)+nCr(n-1,r-1);
+        return nCr(n-1,r)+nCr(n-1,r-1)
 }
 
 function PrintTrangle(n)
 {
-    $("#out").html("");
-    let out="";
+    $("#out").html("")
+    let out=""
     for(let i=1;i<=n+1;i++)
     {
-        for(let a=n;a>i;a--)
+        out+='<div style="display:flex">'
+        for(let a=n;a>=i;a--)
         {
-            out+='&nbsp;';
+            out+='<div style="margin:5px"></div>'
         }
         for(let j=1;j<=i;j++)
         {
-            out+=`&nbsp;${nCr(i-1,j-1)}&nbsp;`;
+            out+=`<div style="margin:5px">${nCr(i-1,j-1)}</div>`
         }
-        out+='<br>';
+        out+='</div>'
     }
-    return out;
+    return out
 }
